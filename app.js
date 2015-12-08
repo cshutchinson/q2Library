083aec082adf9b459383e563a6a100f6d53d800f
Christopher Hutchinson <cshutchinson@gmail.com>
2015-12-08 11:56:06 -0700

    cowsays menu

--- app.js
+++ app.js
@@ -6,11 +6,43 @@ var app = express();
 var books = ["Javascript-The Good Parts", "Eloquent Javascript",
 "Horton Hears a Who"];
 
+var menu = {"menu": [
+  {
+  "id": 1,
+  "type": "burger",
+  "name": "Cheeseburger",
+  "price": 10.99
+  },
+  {
+  "id": 2,
+  "type": "pizza",
+  "name": "Cheese Pizza",
+  "price": 9.99
+  },
+  {
+  "id": 3,
+  "type": "burger",
+  "name": "Hamburger",
+  "price": 8.99
+  },
+  {
+  "id": 4,
+  "type": "pizza",
+  "name": "Pepperoni Pizza",
+  "price": 11.99
+  },
+  {
+  "id": 5,
+  "type": "pizza",
+  "name": "Sausage Pizza",
+  "price": 12.99
+}]};
+
 app.get('/cowsay/:cowsays', function(request, response){
   response.setHeader('content-type','text/html');
   response.send(
     "<pre>" +
-      cowsay.say({'text': request.params.cowsays}) +
+      cowsay.say({'text': JSON.stringify(menu)}) +
     "</pre>"
   );
 })
